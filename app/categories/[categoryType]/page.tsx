f121b77f3e1e99543eac434319160fdea386b3ab
Nazrin Aliyeva <nazrinaliyeva0202@gmail.com>
2023-06-29 16:27:48 +0400

    added params instead of hard-coded type

--- app/categories/[categoryType]/page.tsx
+++ app/categories/[categoryType]/page.tsx
@@ -1,11 +1,7 @@
-// 'use client';
-import {useParams} from "next/navigation";
-import {Category} from "@/types";
-import {categoryTypes} from "@/categoryTypesStorage";
 import CategoryDetail from "@/components/CategoryDetail";
 import {MongoClient} from "mongodb";
 
-export default async function Category() {
+export default async function Category({params}) {
     const uri = 'mongodb+srv://nazrinaliyeva2002:gKnl60gSEp7Yxznw@cluster0.thubz9o.mongodb.net/categories?retryWrites=true&w=majority';
     const dbName = 'categories';
 
@@ -17,19 +13,12 @@ export default async function Category() {
 
     const quotes = await collection.find().toArray();
 
-    // const params = useParams();
-    //
-    // const decodedCategoryType = decodeURIComponent(params.categoryType);
-
     const selectedCategory = quotes.filter(
-        (category) => category.type === "Love"
+        (category) => category.type === params.categoryType
     );
-
 
     return (
         <>
-            {/*<h1>Category: {decodedCategoryType}</h1>*/}
-            {/*<p>{selectedCategory.length > 0 && <CategoryDetail chosenCategory={selectedCategory[0].content} />}</p>*/}
             <div>
                 {selectedCategory.length > 0 && (
                     <CategoryDetail quotes={selectedCategory} />
--- components/CategoryDetail.tsx
+++ components/CategoryDetail.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const CategoryDetail: React.FC = ({quotes}) => {
     return (
-        <div>{quotes.map((quote) => (
-            // eslint-disable-next-line react/jsx-key
-            <p>{quote.quote}</p>
-        ))}</div>
+        <>
+            <div>{quotes.map((quote) => (
+                // eslint-disable-next-line react/jsx-key
+                <p>{quote.quote}</p>
+            ))}</div>
+        </>
     )
 }
 
